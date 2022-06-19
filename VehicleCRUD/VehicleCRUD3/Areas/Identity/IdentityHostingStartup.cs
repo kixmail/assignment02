2e129cef0cad02699a0e80f4f4b2006c9db619ff
Taha Dar <taha.dar@Cdocs.local>
2022-06-19 21:17:35 +0500

    this is my source code

---
+++ VehicleCRUD/VehicleCRUD3/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VehicleCRUD.Data;
+
+[assembly: HostingStartup(typeof(VehicleCRUD.Areas.Identity.IdentityHostingStartup))]
+namespace VehicleCRUD.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
